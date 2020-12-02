2966cff23b91a7d38beef660a79fe6479cb46ac8
Mehedi Hasan <rabbi.se@gmail.com>
2020-12-02 13:14:41 +0600

    Add optimize version of remove duplicate from array.

--- src/coding_interview/src/ch_3/remove_duplicate.cpp
+++ src/coding_interview/src/ch_3/remove_duplicate.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+/* Regular solution */
 std::vector<int> remove_duplicate(int *arr, int length)
 {
     std::vector<int> uniqueNumbers;
@@ -29,6 +30,30 @@ std::vector<int> remove_duplicate(int *arr, int length)
     return uniqueNumbers;
 }
 
+/* Optimized solution */
+int remove_duplicates(int *arr, int length)
+{
+    /* if length 0 then return */
+    if (length == 0)
+        return 0;
+
+    int lastIndex = 0;
+    /* if there are more elements then */
+    if (length > 1)
+    {
+        for (int i = 1; i < length; i++)
+        {
+            if (arr[i - 1] != arr[i])
+            {                           
+                lastIndex++;     
+                arr[lastIndex] = arr[i];                
+            }            
+        }
+    }
+
+    return lastIndex;
+}
+
 int main()
 {
     int arr[] = {1, 1, 2, 3, 3};
@@ -40,6 +65,16 @@ int main()
     {
         /* code */
         cout << uniqueNumbers[i] << " ";
+    }
+
+
+    int lastIndex = remove_duplicates(arr, length);
+
+    cout << endl << "Unique: " << endl;
+    for (int i = 0; i <= lastIndex; i++)
+    {
+        /* code */
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
